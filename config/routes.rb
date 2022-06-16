c7cd4a966f3c291e90a7b6c5bd4c5c5b65e458ee
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-06-16 23:27:25 +0200

    Implement comments. Minor glich renders garbadge

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   get 'users/profile'
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
   get '/u/:id', to: 'users#profile', as: 'user'
-  resources :posts
+
+  resources :posts do
+    resources :comments
+  end
+
   get 'about', to: 'pages#about'
 
   root "pages#home"
